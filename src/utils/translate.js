6dd3724dc71b9145e282195dad2413e9637cfbc6
joshuaxu71 <joshuaxu71@gmail.com>
2024-08-07 06:55:30 +0700

    handle spacing and newline

--- src/utils/translate.js
+++ src/utils/translate.js
@@ -5,12 +5,26 @@ const Hangul = require('hangul-js');
 const { englishToKoreanKeyMap } = require('@mappings/en_kr_map.js');
 
 function translateEnglishToKorean(input) {
-	const disassembled = Hangul.disassemble(input);
-    const translated = []
-    disassembled.forEach(element => {
-        translated.push(englishToKoreanKeyMap[element])
+    newLineOutputs = []
+    newLineInputs = input.split('\n')
+
+    newLineInputs.forEach(input => {
+        spacedOutputs = []
+        spacedInputs = input.split(' ')
+    
+        spacedInputs.forEach(input => {
+            const disassembled = Hangul.disassemble(input);
+            const translated = []
+            disassembled.forEach(element => {
+                translated.push(englishToKoreanKeyMap[element])
+            })
+            spacedOutputs.push(Hangul.assemble(translated));
+        })
+
+        newLineOutputs.push(spacedOutputs.join(' '))
     })
-    return Hangul.assemble(translated);
+	
+    return newLineOutputs.join('\n');
 }
 
 module.exports = {
